34c94f85889e026887d0cde7e7defb0c7e9c894f
sumeshi <sum3sh1@protonmail.com>
2022-03-08 01:53:16 +0900

    feat: split log func

--- src/ntfsdump/views/BaseView.py
+++ src/ntfsdump/views/BaseView.py
@@ -1,8 +1,7 @@
 # coding: utf-8
 import argparse
 from abc import ABCMeta, abstractmethod
-from importlib.metadata import version
-
+from ntfsdump import get_version
 
 class BaseView(metaclass=ABCMeta):
 
@@ -11,13 +10,9 @@ class BaseView(metaclass=ABCMeta):
         self.__define_common_options()
 
     def __define_common_options(self):
-        self.parser.add_argument("--version", "-v", action="version", version=version('ntfsdump'))
+        self.parser.add_argument("--version", "-v", action="version", version=get_version())
         self.parser.add_argument("--quiet", "-q", action='store_true', help="flag to suppress standard output.")
 
     @abstractmethod
     def define_options(self):
         pass
-
-    def log(self, message: str, is_quiet: bool):
-        if not is_quiet:
-            print(message)
