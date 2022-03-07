7276953ba714ea34099abf35117cbdbb813ada55
sumeshi <sum3sh1@protonmail.com>
2022-03-07 21:54:38 +0900

    refactor: clean init file

--- src/ntfsdump/__init__.py
+++ src/ntfsdump/__init__.py
@@ -1,14 +1,20 @@
 # coding: utf-8
-from typing import List, Literal, Optional
+from typing import Literal, Optional
 
 from ntfsdump.presenters.NtfsDumpPresenter import NtfsDumpPresenter
 
-def ntfsdump(imagefile_path: str, output_path: str, target_queries: List[str], volume_num: Optional[int] = None, file_type: Literal['raw', 'e01'] = 'raw'):
+def ntfsdump(
+    imagefile_path: str,
+    output_path: str,
+    target_queries: list[str],
+    volume_num: Optional[int] = None,
+    file_type: Literal['raw', 'e01'] = 'raw'
+):
 
     NtfsDumpPresenter().ntfsdump(
-        imagefile_path=imagefile_path,
-        output_path=output_path,
-        target_queries=target_queries,
-        volume_num=volume_num,
-        file_type=file_type,
+        imagefile_path,
+        output_path,
+        target_queries,
+        volume_num,
+        file_type,
     )
