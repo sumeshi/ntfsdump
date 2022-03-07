c79a0c9fecf21f33b1d0f6a622341567b7b51d43
sumeshi <sum3sh1@protonmail.com>
2022-03-08 02:01:10 +0900

    docs: add docstrings to log class

--- src/ntfsdump/models/Log.py
+++ src/ntfsdump/models/Log.py
@@ -13,7 +13,17 @@ def get_strdatetime() -> str:
 
 
 class Log(object):
-    def __init__(self, path: Path = Path('.', f"{get_program_name()}_{get_strdatetime()}.log"), is_quiet: bool = False):
+    def __init__(
+        self,
+        path: Path = Path('.', f"{get_program_name()}_{get_strdatetime()}.log"),
+        is_quiet: bool = False
+    ):
+        """Logging class
+
+        Args:
+            path (Path, optional): path of log file. Defaults to Path('.', f"{get_program_name()}_{get_strdatetime()}.log").
+            is_quiet (bool, optional): flag to supress standard output. Defaults to False.
+        """
         self.path = path
         self.is_quiet = is_quiet
         self.__create_logfile()
@@ -28,13 +38,28 @@ class Log(object):
         except Exception as e:
             self.print_danger(format_exc())
 
-    def print_info(self, message):
+    def print_info(self, message: str):
+        """print with cyan color
+
+        Args:
+            message (str): a message to be printed.
+        """
         print(f"\033[36m{message}\033[0m")
 
-    def print_danger(self, message):
+    def print_danger(self, message: str):
+        """print with red color
+
+        Args:
+            message (str): a message to be printed.
+        """
         print(f"\033[31m{message}\033[0m")
     
     def log(self, message: str):
+        """print and write message to logfile
+
+        Args:
+            message (str): a message to be logged.
+        """
         self.__write_to_log(message)
         if not self.is_quiet:
            self.print_info(message) 
