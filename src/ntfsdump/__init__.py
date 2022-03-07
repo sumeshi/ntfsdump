92fcbbab3760c83a814744a0eedcef434b9e7e5a
sumeshi <sum3sh1@protonmail.com>
2022-03-07 21:55:22 +0900

    docs: add docstring to ntfsdump func

--- src/ntfsdump/__init__.py
+++ src/ntfsdump/__init__.py
@@ -10,7 +10,15 @@ def ntfsdump(
     volume_num: Optional[int] = None,
     file_type: Literal['raw', 'e01'] = 'raw'
 ):
+    """A tool for extract any files from an NTFS volume on an image file.
 
+    Args:
+        imagefile_path (str): target image file path.
+        output_path (str): output target file path, or output target directory path.
+        target_queries (list[str]): query for extracted file paths.
+        volume_num (Optional[int], optional): system volume number. Defaults to None.
+        file_type (Literal['raw', 'e01'], optional): target image file format. Defaults to 'raw'.
+    """
     NtfsDumpPresenter().ntfsdump(
         imagefile_path,
         output_path,
