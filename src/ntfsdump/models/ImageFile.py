1dcebad3827d6bb1b0fa278ad9807c1b76e1c419
sumeshi <sum3sh1@protonmail.com>
2022-03-10 00:18:23 +0900

    refactor: linebreaks

--- src/ntfsdump/models/ImageFile.py
+++ src/ntfsdump/models/ImageFile.py
@@ -7,11 +7,11 @@ from ntfsdump.models.NtfsVolume import NtfsVolume
 import pytsk3
 import pyewf
 
+
 class ewf_Img_Info(pytsk3.Img_Info):
   def __init__(self, ewf_handle):
     self._ewf_handle = ewf_handle
-    super(ewf_Img_Info, self).__init__(
-        url="", type=pytsk3.TSK_IMG_TYPE_EXTERNAL)
+    super(ewf_Img_Info, self).__init__(url="", type=pytsk3.TSK_IMG_TYPE_EXTERNAL)
 
   def close(self):
     self._ewf_handle.close()
